f0487449d998a62fb944fb2723e771cab2144475
nguyennha <nguyenvannha1910@gmail.com>
2022-08-03 23:48:36 +0700

    update

--- todosite/settings.py
+++ todosite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_FIELD_DIRS=[os.path.join(BASE_DIR,'todosite/static')]
